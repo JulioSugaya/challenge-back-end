b7069d0208cd60b2f2086f19d9e33efc91e7e635
Júlio Mendes Sugaya <juliosugaya@gmail.com>
2019-10-08 15:56:21 -0300

    [AmeDigital][SwPlanetsAPI][Exception] Exception classes

---
+++ SwPlanetsAPIAme/src/main/java/com/amedigital/SwPlanetsAPIAme/exception/PathNotFoundException.java
@@ -0,0 +1,9 @@
+package com.amedigital.SwPlanetsAPIAme.exception;
+
+@SuppressWarnings("serial")
+public class PathNotFoundException extends Exception {
+
+    public PathNotFoundException(final String message) {
+        super(message);
+    }
+}
